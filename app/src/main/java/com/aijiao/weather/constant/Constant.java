6483e121a13cbe03c8207db49ff722a3a585e5f8
aijiao <79963354@qq.com>
2017-04-03 12:00:42 +0800

    数据库配置，实体类创建

---
+++ app/src/main/java/com/aijiao/weather/constant/Constant.java
@@ -0,0 +1,19 @@
+package com.aijiao.weather.constant;
+
+/**
+ * Created by 25858 on 2017/4/3.
+ */
+
+public class Constant {
+
+    /**
+     * 和风天气接口地址
+     */
+    public static String heweather_url = "http://guolin.tech/api/weather?";
+
+    /**
+     * 和风天气接口key
+     */
+    public static String heweather_key = "f101c506e1a346be9ef5b913dce91895";
+
+}
