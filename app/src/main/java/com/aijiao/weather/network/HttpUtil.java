d8ca37c4ead08eb5fb5e59aa8923b9edb07a4d11
aijiao <79963354@qq.com>
2017-04-03 12:26:33 +0800

    请求方法封装，JSON解析封装

---
+++ app/src/main/java/com/aijiao/weather/network/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.aijiao.weather.network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by 25858 on 2017/4/3.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String url, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
